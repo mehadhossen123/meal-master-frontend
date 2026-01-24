ad7b2a54e05d3074b60f6726aa5a502f0edd5e3a
Mehad Hossen <mehad004938@gmail.com>
2026-01-24 18:31:36 +0600

    add google login button and add log

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router";
+import Logo from "../component/Logo";
 
 const Login = () => {
   return (
@@ -13,6 +14,11 @@ const Login = () => {
       >
         {/* Header */}
         <div className="text-center mb-10">
+          <div className="flex justify-center my-2 md:my-5">
+            {" "}
+            <Logo></Logo>
+          </div>
+
           <h2 className="text-3xl font-bold text-gray-800">Welcome Back</h2>
           <p className="text-gray-500 mt-2">Login in your mess account here </p>
         </div>
@@ -63,12 +69,50 @@ const Login = () => {
           >
             Login Now
           </motion.button>
+          {/* google login button */}
+
+          <div className="w-full">
+            {" "}
+            <button className="flex items-center justify-center gap-3 w-full bg-white text-black border border-[#e5e5e5] py-3 rounded-xl hover:bg-gray-50 transition-all font-medium">
+              <svg
+                aria-label="Google logo"
+                width="20"
+                height="20"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <g>
+                  <path d="m0 0H512V512H0" fill="#fff"></path>
+                  <path
+                    fill="#34a853"
+                    d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                  ></path>
+                  <path
+                    fill="#4285f4"
+                    d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                  ></path>
+                  <path
+                    fill="#fbbc02"
+                    d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                  ></path>
+                  <path
+                    fill="#ea4335"
+                    d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                  ></path>
+                </g>
+              </svg>
+              sign up with Google
+            </button>
+          </div>
         </form>
 
         {/* Footer */}
-        <p className="text-center text-gray-600 mt-8">
-         Don't have an account
-          <Link to={"/auth/register"} className="text-blue-600 font-bold hover:underline">
+        <p className="text-center text-gray-600 mt-4">
+          Don't have an account
+          <Link
+            to={"/auth/register"}
+            className="text-blue-600 font-bold hover:underline"
+          >
             Sign Up now
           </Link>
         </p>
--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router";
+import Logo from "../component/Logo";
 
 const Register = () => {
   return (
@@ -13,11 +14,15 @@ const Register = () => {
       >
         {/* Header */}
         <div className="text-center mb-8">
+          <div className="flex justify-center my-2 md:my-5">
+            {" "}
+            <Logo></Logo>
+          </div>
           <h2 className="text-3xl font-bold text-gray-800 font-poppins">
             Create Account
           </h2>
           <p className="text-gray-500 mt-2">
-            আপনার মেস ম্যানেজমেন্ট যাত্রা শুরু করুন
+            Start your mess management journey today
           </p>
         </div>
 
@@ -50,21 +55,10 @@ const Register = () => {
           </div>
 
           {/* Mess Name */}
-          <div className="md:col-span-2">
-            <label className="block text-sm font-semibold text-gray-700 mb-1">
-              Mess Name
-            </label>
-            <input
-              type="text"
-              placeholder="Dream Mess"
-              className="w-full px-4 py-3 rounded-xl border border-gray-200 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 outline-none transition-all"
-              required
-            />
-          </div>
 
           {/* Password */}
-          <div>
-            <label className="block text-sm font-semibold text-gray-700 mb-1">
+          <div className="md:col-span-2">
+            <label className=" block  text-sm font-semibold text-gray-700 mb-1">
               Password
             </label>
             <input
@@ -76,34 +70,62 @@ const Register = () => {
           </div>
 
           {/* Confirm Password */}
-          <div>
-            <label className="block text-sm font-semibold text-gray-700 mb-1">
-              Confirm Password
-            </label>
-            <input
-              type="password"
-              placeholder="••••••••"
-              className="w-full px-4 py-3 rounded-xl border border-gray-200 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 outline-none transition-all"
-              required
-            />
-          </div>
 
           {/* Register Button */}
-          <div className="md:col-span-2 mt-4">
+          <div className="md:col-span-2 ">
             <motion.button
               whileHover={{ scale: 1.02 }}
               whileTap={{ scale: 0.98 }}
-              className="w-full bg-blue-600 text-white font-bold py-3 rounded-xl shadow-lg shadow-blue-200 hover:bg-blue-700 transition-colors"
+              className="w-full bg-secondary cursor-pointer text-white font-bold py-3 rounded-xl shadow-lg shadow-blue-200 hover:bg-blue-700 transition-colors"
             >
               Sign Up Now
             </motion.button>
+          </div>
+
+          {/* google login button */}
+
+          <div className="md:col-span-2 mt-2">
+            {" "}
+            <button className="flex items-center justify-center gap-3 w-full bg-white text-black border border-[#e5e5e5] py-3 rounded-xl hover:bg-gray-50 transition-all font-medium">
+              <svg
+                aria-label="Google logo"
+                width="20"
+                height="20"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <g>
+                  <path d="m0 0H512V512H0" fill="#fff"></path>
+                  <path
+                    fill="#34a853"
+                    d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                  ></path>
+                  <path
+                    fill="#4285f4"
+                    d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                  ></path>
+                  <path
+                    fill="#fbbc02"
+                    d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                  ></path>
+                  <path
+                    fill="#ea4335"
+                    d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                  ></path>
+                </g>
+              </svg>
+              sign up with Google
+            </button>
           </div>
         </form>
 
         {/* Footer */}
         <p className="text-center text-gray-600 mt-6">
           Already have an account?{" "}
-          <Link to={"/auth/login"} className="text-blue-600 font-bold hover:underline">
+          <Link
+            to={"/auth/login"}
+            className="text-blue-600 font-bold hover:underline"
+          >
             Log In now
           </Link>
         </p>
