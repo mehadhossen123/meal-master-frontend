d880dc5ae10b6aea9f01a47eb427a31658298356
Mehad Hossen <mehad004938@gmail.com>
2026-02-04 15:22:45 +0600

    get all expense by tanstack query

--- src/hook/useRole.jsx
+++ src/hook/useRole.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React, { use } from "react";
+import useAxios from "./axios/useAxios";
+import { AuthContext } from "../auth/AuthContext";
 
 const useRole = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const publicAxios = useAxios();
+  const { user } = use(AuthContext);
+  const {data:userRole="",isLoading:roleLoading} = useQuery({
+    queryKey: ["user",user?.email],
+    enabled: !!user?.email,
+    queryFn: async () => {
+      const res = await publicAxios.get(`/user?email=${user?.email}`);
+      return res?.data?.role;
+    },
+  });
+  return {userRole,roleLoading};
 };
 
 export default useRole;
