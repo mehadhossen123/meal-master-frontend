1d7e6c35a9101173f94c47ed1515cdd130cb5d57
Mehad Hossen <mehad004938@gmail.com>
2026-02-13 08:49:03 +0600

    make component MyExpense

--- src/Router/MyExpense.jsx
+++ src/Router/MyExpense.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import useRole from '../hook/useRole';
+import { useQuery } from '@tanstack/react-query';
+import React, { use } from 'react';
+import useAxios from '../hook/axios/useAxios';
+import { AuthContext } from '../auth/AuthContext';
+
 
 const MyExpense = () => {
-    const {userRole}=useRole()
-    console.log("user role ",userRole)
+    const publicAxios=useAxios();
+    const {user}=use(AuthContext)
+    const {data:allExpenses}=useQuery({
+        queryKey:["expenses"],
+        enabled:!!user?.email,
+        queryFn:async ()=>{
+            const res=await publicAxios.get(`/expenses?email=${user?.email}`)
+            return res.data;
+        }
+    })
+   
     return (
         <div className=''>
-            this is my expense 
+            this is my expense :{allExpenses}
             
         </div>
     );
