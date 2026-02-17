a182e40d43ece2ba02bdabfa64c62055afdefc5f
Mehad Hossen <mehad004938@gmail.com>
2026-02-17 17:34:59 +0600

    get all meal data

--- src/Router/MyMeal.jsx
+++ src/Router/MyMeal.jsx
@@ -2,23 +2,29 @@ import { useQuery } from '@tanstack/react-query';
 import React, { use } from 'react';
 import { AuthContext } from '../auth/AuthContext';
 import useAxios from '../hook/axios/useAxios';
+import Loading from '../component/Loading';
 
 const MyMeal = () => {
     const {user}=use(AuthContext)
     const publicAxios=useAxios()
-   const { data: userRole = "", isLoading: roleLoading } = useQuery({
-     queryKey: ["user", user?.email],
+   const { data:meals, isLoading } = useQuery({
+     queryKey: ["meal", user?.email],
      enabled: !!user?.email,
      queryFn: async () => {
-       const res = await publicAxios.get(`/user?email=${user?.email}`);
-       return res?.data?.role;
+       const res = await publicAxios.get(`/meal?email=${user?.email}`);
+       return res?.data;
      },
    });
+   if(isLoading){
+    return <Loading></Loading>
+   }
     return (
-        <div>
-            this is my meal route
-            
-        </div>
+      <div>
+        <h1 className="text-center md:text-4xl text-2xl font-bold mb-8">
+          <span className="text-primary">Total </span> Meal List :
+          <span className="text-secondary">{meals.length}</span>
+        </h1>
+      </div>
     );
 };
 
