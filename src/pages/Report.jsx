897641ccbb206b072da5f522205b6d8ccbd4ce7b
Mehad Hossen <mehad004938@gmail.com>
2026-04-11 23:38:10 +0600

    edit something

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 const Report = () => {
     return (
+      <div className="pt-20">
         <div>
-
-            this is report 
-            
+          <h1 className='lg:text-4xl my-5 text-2xl text-center  font-bold '>
+           
+            Meal Distribution <span className='text-primary'>Overview</span>
+          </h1>
         </div>
+      </div>
     );
 };
 
