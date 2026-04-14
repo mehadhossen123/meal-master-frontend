7a92cf78b02ac6dca075f4f5a5f08f595a59c168
Mehad Hossen <mehad004938@gmail.com>
2026-04-14 09:03:07 +0600

    get all year meal

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -1,6 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import useAxios from '../hook/axios/useAxios';
 
 const Report = () => {
+  const publicAxios=useAxios();
+
+  const {data:yearData}=useQuery({
+    queryKey:[],
+    queryFn:async()=>{
+      const res=await publicAxios.get(`/meal/year`)
+      return res?.data;
+    }
+  })
+  console.log("year data is ",yearData);
     return (
       <div className="pt-20">
         <div>
